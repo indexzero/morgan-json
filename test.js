7eb9f5c5b4d234e58d6c28412a2b0b6fdb5bdd45
indexzero <charlie.robbins@gmail.com>
2016-10-13 19:41:13 -0400

    [test] Add the first test.

---
+++ test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var assume = require('assume');
+var morgan = require('morgan');
+var json = require('./');
+
+//
+// A simple mock "morgan" object which returns deterministic
+// output from the defined functions.
+//
+var mock = {
+  method: function () { return 'method' },
+  url:    function () { return 'url' },
+  status: function () { return 'status' },
+  res:    function (req, res, arg) { return ['res', arg].join(' ') },
+  'response-time': function () { return 'response-time' },
+};
+
+describe('morgan-json', function () {
+  it('format string of all tokens', function () {
+    var compiled = json(':method :url :status :res[content-length] :response-time');
+    var output = compiled(mock);
+
+    assume(output).deep.equals(JSON.stringify({
+      method: 'method',
+      url: 'url',
+      status: 'status',
+      res: 'res content-length',
+      'response-time': 'response-time'
+    }));
+  });
+});
