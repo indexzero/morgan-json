b9e1b2ae5156fa09e948a5d2b12e723ed1d6d556
indexzero <charlie.robbins@gmail.com>
2016-10-19 22:39:04 -0400

    [api test] Finish implementation for both strings and objects with 100% code coverage.

--- index.js
+++ index.js
@@ -13,15 +13,19 @@ var debug = require('diagnostics')('morgan-json');
  * @public
  */
 module.exports = function compile (format) {
+  if (format === '') {
+    throw new Error('argument format string must not be empty');
+  }
+
   if (typeof format !== 'string') {
     return compileObject(format);
   }
 
   var fmt = format.replace(/"/g, '\\"')
   var js = '  "use strict"\n  return JSON.stringify({' + fmt.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?([^:]+)?/g, function (_, name, arg, trail, offset, str) {
-    var tokenName = String(JSON.stringify(name))
-    var tokenArguments = 'req, res'
-    var tokenFunction = 'tokens[' + tokenName + ']'
+    var tokenName = String(JSON.stringify(name));
+    var tokenArguments = 'req, res';
+    var tokenFunction = 'tokens[' + tokenName + ']';
     var trailer = (trail || '').trimRight();
 
     if (arg !== undefined) {
@@ -34,7 +38,7 @@ module.exports = function compile (format) {
   debug('\n%s', js);
 
   // eslint-disable-next-line no-new-func
-  return new Function('tokens, req, res', js)
+  return new Function('tokens, req, res', js);
 }
 
 /**
@@ -51,4 +55,25 @@ function compileObject (format) {
   if (!format || typeof format !== 'object') {
     throw new Error('argument format must be a string or an object');
   }
+
+  var keys = Object.keys(format);
+  var js = '  "use strict"\n  return JSON.stringify({' + keys.map(function (key, i) {
+    var assignment = '\n    "' + key + '": "' + format[key].replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function (_, name, arg) {
+      var tokenArguments = 'req, res';
+      var tokenFunction = 'tokens[' + String(JSON.stringify(name)) + ']';
+
+      if (arg !== undefined) {
+        tokenArguments += ', ' + String(JSON.stringify(arg));
+      }
+
+      return '" + (' + tokenFunction + '(' + tokenArguments + ') || "-") + "';
+    }) + '"';
+
+    return assignment;
+  }) + '\n  })';
+
+  debug('\n%s', js);
+
+  // eslint-disable-next-line no-new-func
+  return new Function('tokens, req, res', js);
 };
